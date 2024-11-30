64af6e139b702eb5b9ebf835c3a753147491ec47
PerccyKing <perccyking@gmail.com>
2024-11-30 13:38:39 +0800

    :memo: Writing docs.

--- about/ezasse-doc/docusaurus.config.ts
+++ about/ezasse-doc/docusaurus.config.ts
@@ -40,7 +40,7 @@ const config: Config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+            'https://github.com/PerccyKing/ezasse/about/ezasse-doc/docs/',
           lastVersion: 'current',
           versions: {
             current: {
@@ -59,7 +59,7 @@ const config: Config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+            'https://github.com/PerccyKing/ezasse/about/ezasse-doc/blog/',
           // Useful options to enforce blogging best practices
           onInlineTags: 'warn',
           onInlineAuthors: 'warn',
