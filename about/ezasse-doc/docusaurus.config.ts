4b97a9ccb229e241349f40491fa72065a7f15e6b
PerccyKing <perccyking@gmail.com>
2024-11-30 15:01:15 +0800

    按发布环境构建

--- about/ezasse-doc/docusaurus.config.ts
+++ about/ezasse-doc/docusaurus.config.ts
@@ -10,10 +10,10 @@ const config: Config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://ezassse.pism.com.cn',
+  url: process.env.DEPLOY_URL || 'https://ezassse.pism.com.cn',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: process.env.BASE_URL || '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
